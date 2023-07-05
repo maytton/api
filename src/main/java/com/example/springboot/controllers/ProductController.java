a49a4a3380dc468b6fcea704359bc267e27b217a
maytton <maytton0000@gmail.com>
2023-07-04 21:52:40 -0300

    Created the method to save the data in the database.

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,7 +1,17 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.dto.ProductRecordDTO;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import jakarta.validation.Valid;
+import org.apache.catalina.mbeans.MBeanUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +20,12 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 
 }
